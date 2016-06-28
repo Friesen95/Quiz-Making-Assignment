35418d67eca6e31500af4eab08cd29d7f6346b39
Friesen95 <friesen.alex95@gmail.com>
2016-06-28 17:00:15 -0400

    Made the proper changes so that they main method that runs the code is next to empty

--- Quiz-Making-Assignment/Source.cpp
+++ Quiz-Making-Assignment/Source.cpp
@@ -40,54 +40,13 @@ int main(){
 		/*
 		StartQuiz (Alex and Emma)
 		- sends first nested vector to a method called Find answer
-		- stores the answer in a varible
+		- stores the answer in a variable
 		- Goes through array and starts to post the question and possible answers in all lower case
 		- give them the option to pick answer 1,2,3, or 4
-		- if they pick correct then add 1 tothe score if not then continue for loop
+		- if they pick correct then add 1 to the score if not then continue for loop
 		- at the end of loop return final score
 		*/
-			int score = 0;
-			string choice;
-			char choiceChar;
-			vector<char> answers;
-			answers = answer(quiz);
-			string toPrintLower = "";
-			string toPrint = "";
-			for (int q = 0; q < quiz.size(); q++) {
-				for (int a = 0; a < quiz[q].size(); a++) {
-					if (a == 0) {
-						cout << quiz[q][a];
-					}
-					else {
-						toPrintLower = quiz[q][a];
-						toPrint = toLowerCase(toPrintLower);
-						cout << toPrint;
-					}
-					cout << "\n";
-				}
-				cout << "Please enter your answer as the letter you believe is correct.\n";
-				// Right after this line get an input/answer from the user to the question 
-				getline(cin, choice);
-				choice = toUpperCase(choice);
-				//Prompt for valid choice until given
-				while (choice != "A" && choice != "B" && choice != "C" && choice != "D") {
-					cout << "Please enter a, b, c, or d.";
-					cout << "Please enter your answer as the letter you believe is correct.\n";
-					getline(cin, choice);
-					choice = toUpperCase(choice);
-				}
-				choiceChar = choice[0];
-				//If what they chose == to the answer than add one point
-				if (choiceChar == answers[q]) {
-					score++;
-				}
-			}
-
-			for (int x = 0; x < answers.size(); x++) {
-				cout << answers[x] << "\n";
-			}
-
-			cout << score;
+		startQuiz(quiz);
 
 
 		//"mark" the quiz
--- Quiz-Making-Assignment/quiz.h
+++ Quiz-Making-Assignment/quiz.h
@@ -72,35 +72,55 @@ vector< vector<string> > getInfo(string txtFileName, string typeOfTxt)
 /*
 	StartQuiz (Alex and Emma)
 	- sends first nested vector to a method called Find answer
-	- stores the answer in a varible
+	- stores the answer in a variable
 	- Goes through array and starts to post the question and possible answers in all lower case 
 	- give them the option to pick answer 1,2,3, or 4 
-	- if they pick correct then add 1 tothe score if not then continue for loop
+	- if they pick correct then add 1 to the score if not then continue for loop
 	- at the end of loop return final score
 */
 int startQuiz(vector<vector<string>> quiz) {
-	int score;
-	vector<vector<string>> quiz;
-	vector <char> answers = answer(quiz);
+	int score = 0;
+	string choice;
+	char choiceChar;
+	vector<char> answers;
+	answers = answer(quiz);
 	string toPrintLower = "";
 	string toPrint = "";
-		for (int q = 0; q < quiz.size(); q++) {
-			for (int a = 0; a < quiz[q].size(); a++) {
-				if (a == 0) {
-					cout << quiz[q][a];
-				}
-				else {
-					toPrintLower = quiz[q][a];
-					toPrint = answersToLower(toPrintLower);
-					cout << toPrint;
-				}
-				cout << "\n";
+	for (int q = 0; q < quiz.size(); q++) {
+		for (int a = 0; a < quiz[q].size(); a++) {
+			if (a == 0) {
+				cout << quiz[q][a];
 			}
-			cout << "Please enter your answer as the letter you believe is correct.\n";
-			// Right after this line get an input/answer from the user to the question 
-
-			// if what they chose == to the answer than add one point
+			else {
+				toPrintLower = quiz[q][a];
+				toPrint = toLowerCase(toPrintLower);
+				cout << toPrint;
+			}
+			cout << "\n";
 		}
+		cout << "Please enter your answer as the letter you believe is correct.\n";
+		// Right after this line get an input/answer from the user to the question 
+		getline(cin, choice);
+		choice = toUpperCase(choice);
+		//Prompt for valid choice until given
+		while (choice != "A" && choice != "B" && choice != "C" && choice != "D") {
+			cout << "Please enter a, b, c, or d.";
+			cout << "Please enter your answer as the letter you believe is correct.\n";
+			getline(cin, choice);
+			choice = toUpperCase(choice);
+		}
+		choiceChar = choice[0];
+		//If what they chose == to the answer than add one point
+		if (choiceChar == answers[q]) {
+			score++;
+		}
+	}
+
+	for (int x = 0; x < answers.size(); x++) {
+		cout << answers[x] << "\n";
+	}
+
+	cout << score;
 		return score; 
 }
 
